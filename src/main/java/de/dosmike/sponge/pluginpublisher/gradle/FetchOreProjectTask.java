079f767e9e7c4dace8d67b1ae38c5a2974dbe5a3
DosMike <xanatosefan@gmx.de>
2020-12-18 23:35:44 +0100

    No-Fail Fetch Project Task + I don't think i understand output properties quite yet

--- src/main/java/de/dosmike/sponge/pluginpublisher/gradle/FetchOreProjectTask.java
+++ src/main/java/de/dosmike/sponge/pluginpublisher/gradle/FetchOreProjectTask.java
@@ -2,10 +2,10 @@ package de.dosmike.sponge.pluginpublisher.gradle;
 
 import com.google.gson.JsonObject;
 import de.dosmike.sponge.pluginpublisher.tasks.TaskFunctors;
-import de.dosmike.sponge.pluginpublisher.tasks.TaskRunException;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.StopExecutionException;
 import org.gradle.api.tasks.TaskAction;
 
 public class FetchOreProjectTask extends DefaultTask {
@@ -14,12 +14,17 @@ public class FetchOreProjectTask extends DefaultTask {
 	public Property<String> apiKey = getProject().getObjects().property(String.class);
 	@Input
 	public Property<String> projectId = getProject().getObjects().property(String.class);
-	@Input
+
 	public Property<JsonObject> projectData = getProject().getObjects().property(JsonObject.class);
 
 	@TaskAction
-	public void publish() throws TaskRunException {
-		projectData.set(TaskFunctors.runOreProjectLookup(apiKey.get(), projectId.get()));
+	public void fetch() {
+		try {
+			projectData.set(TaskFunctors.runOreProjectLookup(apiKey.get(), projectId.get()));
+		} catch (Throwable t) {
+			projectData.set(new JsonObject());
+			throw new StopExecutionException(t.getMessage());
+		}
 	}
 
 }
